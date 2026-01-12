cab78d765c14554f2eaad9e974e75f81e60654a8
SpektrunBackend <spektrunbackend@gmail.com>
2026-01-12 20:31:37 +0100

    V.1.2.0

--- config.js
+++ config.js
@@ -4,11 +4,14 @@ export const CONFIG = {
     "llama-3.3-70b-versatile",
     "llama-3.1-8b-instant",
     "moonshotai/kimi-k2-instruct-0905",
+    "openai/gpt-oss-120b",
+    "openai/gpt-oss-20b",
+    "qwen/qwen3-32b"
   ],
 };
 
 export const MAX_LIMITS = {
-  "/api/comment-remover": 15_000,
+  "/api/comment-remover": 19_000,
   "/api/comment-adder": 15_000,
   "/api/optimizer": 15_000,
   "/api/detector": 8_000,
